61171e1be0be4b1f7967cfdfcf5e4107843e6395
ka1serrr <86408343+ka1serrr@users.noreply.github.com>
2022-08-29 17:54:46 +0300

    add api functions

---
+++ src/services/MarvelService.jsx
@@ -0,0 +1,39 @@
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=c6fdacb47e121abae4e838ca5b7f36ce'
+
+    getResource = async (url) => {
+        let res = await fetch (url)
+
+        if (!res.ok) {
+            console.log(`Could not fetch ${url} error ${res.status} occured`)
+        }
+
+
+        return await res.json();
+    }
+    
+
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharatcher);
+    }
+
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharatcher(res.data.results[0]);
+    }
+
+    _transformCharatcher  = (char) => {
+        return {
+            name: char.name,
+            description : char.description ? `${char.description.slice(0, 210)}...` : 'There is no description yet',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url,
+        }
+    }
+}
+
+
+export default MarvelService;
